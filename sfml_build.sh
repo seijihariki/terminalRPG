aeaca8bac6d34a4196eb431d19fcb04904d9aae3
Victor Seiji Hariki <victorseijih@gmail.com>
2016-10-03 15:18:11 -0300

    Just a bit more UI

--- sfml_build.sh
+++ sfml_build.sh
@@ -1,4 +1,7 @@
-sudo apt-get install libjpeg-dev libxrandr-dev libfreetype6-dev libX11-dev libx11-xcb-dev libxcb-image0-dev libxcb-randr0-dev libxcb1-dev libflac-dev libogg-dev libvorbis-dev libvorbisenc2 libvorbisfile3 libopenal-dev libpthread-stubs0-dev -y
+git submodule update --init
+echo "Will now check for dpkg installed libs."
+echo "Press ctrl+C in case you don't have sudo access."
+sudo apt-get install libjpeg-dev libxrandr-dev libfreetype6-dev libX11-dev libx11-xcb-dev libxcb-image0-dev libxcb-randr0-dev libxcb1-dev libflac-dev libogg-dev libvorbis-dev libvorbisenc2 libvorbisfile3 libopenal-dev libpthread-stubs0-dev
 
 cd external/SFML
 cmake -DCMAKE_BUILD_TYPE=release
