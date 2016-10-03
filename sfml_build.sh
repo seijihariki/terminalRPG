36e753b6e0448851753bd30f82d81966f7f6ebe2
Victor Seiji Hariki <victorseijih@gmail.com>
2016-10-03 15:09:52 -0300

    Added sfml build script.

--- sfml_build.sh
+++ sfml_build.sh
@@ -1,4 +1,4 @@
-sudo apt-get install libxrandr-dev -y
+sudo apt-get install libjpeg-dev libxrandr-dev libfreetype6-dev libX11-dev libx11-xcb-dev libxcb-image0-dev libxcb-randr0-dev libxcb1-dev libflac-dev libogg-dev libvorbis-dev libvorbisenc2 libvorbisfile3 libopenal-dev libpthread-stubs0-dev -y
 
 cd external/SFML
 cmake -DCMAKE_BUILD_TYPE=release
